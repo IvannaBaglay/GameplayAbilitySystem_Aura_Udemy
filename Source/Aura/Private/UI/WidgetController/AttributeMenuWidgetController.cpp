f267d67f6bfe470cdb06441ede374905cb8bf236
IvannaBaglay <ibaglaystud@ukr.net>
2026-03-24 17:26:58 +0200

    Check broadcast initial values and fix GameplayTags assign

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -2,10 +2,20 @@
 
 
 #include "UI/WidgetController/AttributeMenuWidgetController.h"
+#include "AbilitySystem/Data/AttributeInfo.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+#include "AuraGameplayTags.h"
 
 void UAttributeMenuWidgetController::BroadcastInitialValues()
 {
+    UAuraAttributeSet* AS = CastChecked<UAuraAttributeSet>(AttributeSet);
 
+    check(AttributeInformation);
+
+    FAuraAttributeInfo Info = AttributeInformation->FindAttributeInfoForTag(FAuraGameplayTags::Get().Attributes_Primary_Strength);
+    Info.AttributeValue = AS->GetStrength();
+
+    AttributeInfoDelegate.Broadcast(Info);
 }
 
 void UAttributeMenuWidgetController::BindCallbacksToDependencies()
